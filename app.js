ac1292434bdcd88e329408bff5d8550567f40272
Hossain <arafat.kd99@gmail.com>
2023-07-04 10:59:09 +0100

    Update header for desktop

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Humbarger Menu
+const nav = document.querySelector('nav');
 const menu = document.querySelector('#menu');
 const humbargerIcon = document.querySelector('.fa-solid');
+const cancelIcon = document.querySelector('.fa-xmark');
 
-function show () {
+function show() {
   menu.style.display = 'flex';
   menu.style.flexDirection = 'column';
   humbargerIcon.style.display = 'none';
+  cancelIcon.style.display = 'flex';
 }
+
+function hide() {
+  menu.style.display = 'none';
+  cancelIcon.style.display = 'none';
+  humbargerIcon.style.display = 'flex';
+}
+
 humbargerIcon.addEventListener('click', show);
+cancelIcon.addEventListener('click', hide);
