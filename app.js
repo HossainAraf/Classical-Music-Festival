d1c68cb940ec199d58b57263cfeda1e89268ae64
Hossain <arafat.kd99@gmail.com>
2023-07-05 15:09:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,19 +22,38 @@ cancelIcon.addEventListener('click', hide);
 // Featured speakers
 const featuredContainer = document.querySelector('#featured-speakers');
 
-let features = [
+const featuresList = [
   {
     id: 0,
     photo: 'img/bonna.jpg',
     name: 'Rezwana Choudhury Bannya',
     desig: 'Bangladeshi singer and academic. She is an exponent of Rabindra Sangeet.',
-    desc: ' She is regarded as a legendary Tagore singer by Tagore music aficionados all around the world.'
+    desc: ' She is regarded as a legendary Tagore singer by Tagore music aficionados all around the world.',
   },
   {
     id: 0,
-    photo: 'img/bonna.jpg',
+    photo: 'img/Nicky.jpg',
     name: 'Nicola Joy Nadia Benedetti',
     desig: 'Italian-Scottish classical solo violinist and festival director.',
     desc: 'She works with orchestras in Europe and America as well as with Alexei Grynyuk, her regular pianist. Since 2012.',
   },
-]
+];
+
+let featureGenerator = '';
+
+featuresList.forEach((feature) => {
+  featureGenerator += `
+  <div class='speakers'>
+  <div class='speakers-photo'>
+    <img src='${feature.photo}' alt='${feature.name}'>
+  </div>
+  <div class='speakers-detail'>
+    <h4>${feature.name}</h4>
+    <h6>${feature.desig}</h6>
+    <p>${feature.desc}</p>
+  </div>
+  </div>
+  `;
+});
+
+featuredContainer.innerHTML = featureGenerator;
